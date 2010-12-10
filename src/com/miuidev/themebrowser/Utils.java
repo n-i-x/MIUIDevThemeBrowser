944cfe4b0a9240c2239405bd8fa6ed69602e4564
n-i-x <nix@innocent.com>
2010-12-09 22:30:57 -0500

    Fix theme size display

--- src/com/miuidev/themebrowser/Utils.java
+++ src/com/miuidev/themebrowser/Utils.java
@@ -4,18 +4,18 @@ import android.content.Context;
 
 public class Utils {
 
-    public static String formatSize(Context mContext, long sizeInBytes) {
+    public static String formatSize(Context mContext, Float sizeInBytes) {
 
     	String[] sizes = mContext.getResources().getStringArray(R.array.sizes);
     	String readableFileSize = null;
-    	
+    
     	for(String size : sizes) {
-    		if (sizeInBytes < 1024.0) {
-    			readableFileSize = Long.toString(sizeInBytes) + size;
+    		if (sizeInBytes < 1024) {
+    			readableFileSize = String.format("%.2f%s", sizeInBytes, size);
     			break;
     		}
-    		
-    		sizeInBytes /= 1024.0;
+    	
+    		sizeInBytes /= 1024;
     	}
     	
     	return readableFileSize;
